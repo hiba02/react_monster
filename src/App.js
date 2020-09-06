1a19f05190cec98623940958c1fa4d51d6abed71
soon <soon@soons-MacBook-Air.local>
2020-09-05 22:22:12 -0400

    card filter implement by filter

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import './App.css';
-import { CardList } from './components/card-list/card-list.component';
-
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      monsters: [
-
-      ]
-    }
-  }
-  
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    // .then(users => console.log(users))
-    .then(users => this.setState({ monsters: users}))
+      monsters: [],
+      searchField: "",
+    };
   }
 
-  render(){
-    return(   
-    <div className="App">
-      <CardList name="YoonSoon">
-        {/* <h1>prop's children</h1> */}
-      {
-        this.state.monsters.map(
-        monster => <h1 key={monster.id}>{ monster.name }</h1>
-        )
-      }
-        </ CardList>
-    </div>
-    )
-  };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      // .then(users => console.log(users))
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
