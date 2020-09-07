51042c26bfbf2e1721d6ab4c104c5e29e985e98c
soon <soon@soons-MacBook-Air.local>
2020-09-07 19:56:14 -0400

    Updates

---
+++ precache-manifest.6293d91ff0b59228d3b4a6e0f1f998f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d858195256a678d9b6f9925b15d179d",
+    "url": "/react_monster/index.html"
+  },
+  {
+    "revision": "2eef8bbbbb5e1f875ba9",
+    "url": "/react_monster/static/css/main.65647089.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/react_monster/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_monster/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eef8bbbbb5e1f875ba9",
+    "url": "/react_monster/static/js/main.19349531.chunk.js"
+  },
+  {
+    "revision": "9e3148b19b89d4c095ce",
+    "url": "/react_monster/static/js/runtime-main.0ace9ec0.js"
+  }
+]);
