e46a633467c17325a9c66402b18562db8c86e7b5
soon <soon@soons-MacBook-Air.local>
2020-09-06 22:49:35 -0400

    binding this in function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
       searchField: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,6 +21,14 @@ class App extends React.Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  // handleChange(e) {
+  //   this.setState({ searchField: e.target.value });
+  // }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -25,12 +36,9 @@ class App extends React.Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="search monsters"
-          onChange={(e) => {
-            this.setState({ searchField: e.target.value });
-          }}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
